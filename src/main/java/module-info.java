87337d92d8010c23963c891b25d36abda5dd8ff8
joseantoniols1212 <joseantonioluquesalguero1212@gmail.com>
2025-07-18 09:48:32 +0200

    fix(BattleEngine): changed fight method. Now it creates the character clones before applying damage to correctly display character info before fight.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,8 +7,6 @@ module com.bosonit.project.lotrjfx {
 
     opens com.bosonit.project.lotrjfx to javafx.fxml;
     exports com.bosonit.project.lotrjfx;
-    exports com.bosonit.project.lotrjfx.core;
-    opens com.bosonit.project.lotrjfx.core to javafx.fxml;
     exports com.bosonit.project.lotrjfx.controller;
     opens com.bosonit.project.lotrjfx.controller to javafx.fxml;
     exports com.bosonit.project.lotrjfx.model.services;
